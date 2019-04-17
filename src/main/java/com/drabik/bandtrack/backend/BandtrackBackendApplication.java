623c2467685b3643a8f788b05e7d7fd3c2b85559
Peter Drabik <drabik@wds.sk>
2019-04-17 15:57:16 +0200

    Initial Spring Boot project

---
+++ src/main/java/com/drabik/bandtrack/backend/BandtrackBackendApplication.java
@@ -0,0 +1,13 @@
+package com.drabik.bandtrack.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BandtrackBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BandtrackBackendApplication.class, args);
+	}
+
+}
